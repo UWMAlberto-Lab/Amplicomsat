89b2ff8b351d4d9bd8168011f5e34de9a6c318ac
Filipe Alberto <63799474+falberto73@users.noreply.github.com>
2025-05-02 10:46:56 -0700

    Add files via upload
    Solving issue when reads have high quality and no reads were to be deleted

--- R/read.quality.filter.R
+++ R/read.quality.filter.R
@@ -11,14 +11,21 @@ pdf(file="Distribution of bases below Q threshold quality.pdf")
 for(f in 1:length(files)){
   sampleTest<-readLines(paste0(sampleDir,"/",files[f]))
   if(length(sampleTest)==0)next
-  sampleTest[seq(4,length(sampleTest),4)]
 
-  QualitySearch<-grep(sampleTest[seq(4,length(sampleTest),4)],pattern=PhredString.T)
   QualitySearch<-lapply(strsplit(sampleTest[seq(4,length(sampleTest),4)],split=""),
   		function(x){grepl(x,pattern=PhredString.T)})
       
   QualitySearch.SUM<-sapply(QualitySearch,sum)
   QualitySearch.R<-which(QualitySearch.SUM>All.below.Q)
+  
+  if(identical(QualitySearch.R, integer(0))){
+  	readr::write_lines(sampleTest,file=paste0("./",filterDir,"/",sub(".fastq",".F.fastq",files[f])))
+  	print(paste0("Filtered file ", files[f]," - ",length(QualitySearch.R)," reads removed"))
+  	readr::write_lines(paste0(files[f]," - ",length(QualitySearch.R),"(",round((length(QualitySearch.R)/(length(sampleTest)/4)*100),2),"%) reads removed")
+  										 ,append = T,file="FilterLog")
+  	hist(QualitySearch.SUM,	xlab="Number of bases below Q threshold quality",
+  			 main=paste0(files[f]," Q threshold: ",Phred.threshold))
+  }else{
    
   QR2remove<-QualitySearch.R*4
   QR2removeALL<-c(QR2remove-3,QR2remove-2,QR2remove-1,QR2remove)
@@ -27,9 +34,9 @@ for(f in 1:length(files)){
   print(paste0("Filtered file ", files[f]," - ",length(QualitySearch.R)," reads removed"))
   readr::write_lines(paste0(files[f]," - ",length(QualitySearch.R),"(",round((length(QualitySearch.R)/(length(sampleTest)/4)*100),2),"%) reads removed")
               ,append = T,file="FilterLog")
-  hist(QualitySearch.SUM,breaks=c(0:max(QualitySearch.SUM)),xlim=c(0,20),
-  	xlab="Number of bases below Q threshold quality",
+  hist(QualitySearch.SUM,	xlab="Number of bases below Q threshold quality",
   	main=paste0(files[f]," Q threshold: ",Phred.threshold))
+  }
   }
   dev.off()
 }
